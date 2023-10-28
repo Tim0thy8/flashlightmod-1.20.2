68d1c49a9ad390eded723d88774720804c2f6946
Tim0thy8 <Timotheee8@gmail.com>
2023-10-28 12:26:39 -0700

    Initial commit

---
+++ src/main/java/com/timothy8/flashlightmod/FlashlightModClient.java
@@ -0,0 +1,10 @@
+package com.timothy8.flashlightmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FlashlightModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
